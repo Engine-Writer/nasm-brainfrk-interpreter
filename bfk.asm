616f837aa03ac6eb16edd85e987aafd8cca788fc
Engine-Writer <193863593+Engine-Writer@users.noreply.github.com>
2026-09-08 15:27:44 +0300

    Refactor memory allocation and interpreter logic

--- bfk.asm
+++ bfk.asm
@@ -1,71 +1,35 @@
 section .bss
-statbuf: 
-  resb 144
-
-section .data
-; Allocate 2 pages of RAM for BrFK interpreter
-; BTW must be init at 0 so not BSS
-myMemory:
-  times 8192 db 0  ; 4096 * 2
+myMemory: resb 8192  ; 4096 * 2. approx 2 pages
+statbuf: resb 144
 
 section .text
 global _start
 
-; Takes in [RSI] as POINTER to str and RCX as MAX length
-; returns strlen as RCX
-; ... I was too lazy to stack frame XD
-strnlen:
-    xchg rsi, rdi  ; RSI makes WAY MORE SENSE for scasb than RDI IMO but cursed intel microcode
-    push rax
-    push rdi
-    push rdi
-
-    ; Actual code
-    xor al, al
-    repne scasb  ; scan [RDI] for null byte
-    
-    pop rax
-    mov rcx, rdi
-    sub rcx, rax
-    dec rcx
-
-    ; restore
-    pop rdi
-    pop rax
-
-    xchg rsi, rdi
-    ret
-
-
 _start:
-    mov rax, [rsp]          ; argc
+    mov rax, [rsp]  ; argc
     cmp rax, 2
-    jl  end_process
+    jl  end_process  ; Error- No Code
 
-    mov rsi, [rsp+16]      ; argv[1] = file path
+    mov rsi, [rsp+16]  ; argv[1] = file path
 
-    ; open(argv[1], O_RDONLY, 0)
-    mov rax, 2
+    mov rax, 2  ; open(argv[1], O_RDONLY, 0)
     mov rdi, rsi
     xor rsi, rsi
     xor rdx, rdx
     syscall
     test rax, rax
     js  end_process
-    mov r12, rax           ; r12 = fd
+    mov r12, rax  ; r12 = fd
 
-    ; fstat(fd, &statbuf)
-    mov rax, 5
+    mov rax, 5  ; fstat(fd, &statbuf)
     mov rdi, r12
     lea rsi, [rel statbuf]
     syscall
     test rax, rax
-    js  end_process
+    js end_process
 
-    mov r13, [rel statbuf + 48]   ; r13 = file size (BF source length)
-
-    ; mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0)
-    mov rax, 9
+    mov r13, [rel statbuf + 48]  ; r13 = file size (BF source length)
+    mov rax, 9  ; mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0)
     xor rdi, rdi
     mov rsi, r13
     mov rdx, 1
@@ -75,41 +39,31 @@ _start:
     syscall
     test rax, rax
     js  end_process
-    mov r14, rax           ; r14 = pointer to BF source in memory
+    mov r14, rax  ; r14 = pointer to BF source in memory
 
-    ; fd no longer needed
-    mov rax, 3
+    mov rax, 3  ; fd no longer needed
     mov rdi, r12
-    syscall
-
-    ; r14 = BF source pointer, r13 = BF source length`
-
-
-    ; Set flags to the only acceptable state
-    cld
-    clc
-    
-    mov r8, 0
+    syscall  ; r14 = BF source pointer, r13 = BF source length
+    xor r8, r8
     mov r9, r14
-    lea r10, [r14 + r13]  ; one of the ways to do fast arithmatic of all time
+    lea r10, [r14 + r13]  ; ... one of the ways to do fast arithmatic of all time
 
     call main_interpreter_fn
     jmp end_process
 
 main_interpreter_fn:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 8
-    mov [rbp - 8], r10
+    ; RBP stack frame omitted for better perf
     push r9
 
 .main_interpreter_loop:
-    lea rdi, [r9]
-    and r8, 0x1FFF
+    cmp r9, r10
+    jae .return_end_fncshn
+
+    and r8, 0x1FFF  ; fym talkin bout some "aSseMBly iSN'T meMORY sAfE"
     lea rsi, [myMemory + r8]
-    mov al, [rdi]
-    
-    ; BIG ASS SWITCH-CASE. INCOMING!
+    mov al, [r9]
+
+    ; BIG ASS IF-ELSE. INCOMING! (less bytes than switch-case ig /shrug)
 .next_case0:
     cmp al, '<'
     jne .next_case1
@@ -144,79 +98,56 @@ main_interpreter_fn:
 
     ; welp
     mov ah, [rsi]
-    test ah, 0xFF
-    jz .end_of_all_cases
+    test ah, ah
+    jz .passthru
     inc r9
     call main_interpreter_fn
     jmp .end_of_all_cases
+.passthru: mov rcx, 1
+.passthru_loop: inc r9
+    cmp r9, r10
+    jae .return_end_fncshn
+    cmp byte [r9], '['
+    jne .skipIncRCX
+    inc rcx
+.skipIncRCX: cmp byte [r9], ']'
+    jne .skipDecRCX
+    dec rcx
+.skipDecRCX: test rcx, rcx
+    jnz .passthru_loop
+    jmp .end_of_all_cases
+
 .next_case5:
     cmp al, ']'
     jne .next_case6
 
     mov ah, [rsi]
-    test ah, 0xFF
+    test ah, ah
     jz .return_end_fncshn
 
-    mov r9, [rbp-16] ; Loop the f*** back
+    mov r9, [rsp] ; Loop the f*** back
     jmp .main_interpreter_loop  ; YOU HAVE NO IDEA HOW FUCKING ANNOYING THIS WAS TO DEBUG
 
-
-.next_case6:
+.next_case6:  ; Wouldn't it be crazy if I got all the registers perfectly setup for the next 2 cases... oh wait
     cmp al, '.'
     jne .next_case7
-    
-    ; Wouldnt it be so funny if all of the parameters are magically set correctly?
-    ; Oh wait
-    call .dotfunction  ; get it?? cuz this is the `.` case and the fn starts with a `.`? not funny? ok
+    mov rax, 1  ; sys_write
+    mov rdi, 1  ; stdout FD
+    syscall
     jmp .end_of_all_cases
 .next_case7:
     cmp al, ','
     jne .end_of_all_cases
+    xor rax, rax  ; sys_read
+    xor rdi, rdi  ; fd = stdin
+    syscall  ; its like the stars aligned
 
-    call .commafunction
+.end_of_all_cases:  inc r9
+    jmp .main_interpreter_loop
 
-
-.end_of_all_cases:
-    inc r9
-    cmp r9, r10
-    jb .main_interpreter_loop
-
-.return_end_fncshn:
-    add rsp, 16
-    pop rbp
+.return_end_fncshn:  add rsp, 8
     ret
 
-.dotfunction:  ; PLEASE SET RSI
-    push rdx
-    push rax
-    push rdi
-
-    mov rdx, 1  ; length
-    mov rax, 1  ; sys_write
-    mov rdi, 1  ; stdout FD
-    syscall
-    
-    pop rdi
-    pop rax
-    pop rdx
-    ret
-
-.commafunction:  ; PLEASE SET RSI
-    push rdx
-    push rax
-    push rdi
-
-    mov rdx, 1  ; length = 1 byte
-    mov rax, 0  ; sys_read
-    mov rdi, 0  ; fd = stdin
-    syscall
-    
-    pop rdi
-    pop rax
-    pop rdx
-    ret
-
-end_process:
-    mov rax, 60
+end_process:  mov rax, 60
     xor rdi, rdi
     syscall
